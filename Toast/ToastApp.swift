1e71c0935faf4a4fd19c09d7fb738040da1469d1
Azhagusundaram.t <Azhagusundaram.t@zohocorp.com>
2021-01-29 10:05:11 +0530

    Initial Commit

---
+++ Toast/ToastApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToastApp.swift
+//  Toast
+//
+//  Created by Azhagusundaram Tamil on 29/01/21.
+//
+
+import SwiftUI
+
+@main
+struct ToastApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
